3966cd458d406f51f060aeac9313acfd514d3050
Shanta Islam <shantoislam7363@gmail.com>
2023-09-15 00:51:13 +0600

    Card section done and also implemented API on each card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react'
 import './App.css'
+import Cards from './components/Cards/Cards'
+import Cart from './components/Cart/Cart'
+import Header from './components/Header/Header'
+import { useEffect } from 'react'
 
 function App() {
+  const [courses, setCourses] = useState([]);
+  const [coursesName, setCoursesName] = useState([]);
 
+  useEffect(() => {
+    fetch('courses.json')
+      .then(res => res.json())
+      .then(data => setCourses(data))
+  }, [])
+
+const handleSelect = course_name => {
+  // console.log("Kya Bhai,Click kar diya",course_name);
+}
   return (
     <>
-    <h2 className='text-4xl text-center'>Course Registration</h2>
-      
+      <Header></Header>
+      <div className='ml-10 flex'>
+        <div className='grid grid-cols-3 gap-4'>
+          {
+            courses.map((course,idx) => <Cards key={idx} handleSelect={handleSelect} course={course}></Cards>)
+          }
+        </div>
+        <div className='w-72 ml-5'>
+          <Cart></Cart>
+        </div>
+      </div>
+
+
+
     </>
   )
 }
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div className="cart-container bg-gray-300">
+            <h2 className="pl-4 ">Credit Hour Remaining </h2>
+            <hr />
+            <h2>akasdmk</h2>
+        </div>
+    );
+};
+
+export default Cart;
